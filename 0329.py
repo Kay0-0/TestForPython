dc6e92698754923ca7f0442ea76e045db8a5675f
4강 <ab@cd.ef>
2023-03-29 16:43:09 +0900

    commit

--- 0329.py
+++ 0329.py
@@ -1,4 +1,5 @@
 #0329
+'''
 print("안녕하세요.")
 print(3)
 print(10.6)
@@ -18,6 +19,8 @@ radius = 3.14
 import keyword
 print(keyword.kwlist)
 # 변수명에 사용할 수 없는 키워드.
+'''
+
 '''
 a = 3
 b = 10
@@ -48,6 +51,7 @@ str(30)
 등
 '''
 
+'''
 planet = input("원하는 행성은? ")
 strRadius = input(planet + " 반지름은? ")
 radius = int(strRadius)
@@ -55,3 +59,56 @@ radius = int(strRadius)
 length = 2*3.14*radius
 print(planet,'반지름:',radius)
 print(planet,'둘레길이:',length)
+'''
+
+'''
+a = "Python"
+print(a[0], "  ", a[1], "...", a[5])
+print("python"[0], "... ", "python"[1], "... ", "python"[5])
+print("동양미래대학교"[0])
+print(len(a))
+
+sch="동양미래대학교 컴퓨터소프트웨어공학과"
+print(sch[:])
+print(sch[:3])
+print(sch[3:])
+print(sch[2:4])
+print(sch[-4:-1])
+print(sch[0:3:2])
+print(sch[0:len(sch):2])
+
+print(sch[8::2])
+print(sch[8:])
+print(sch[:-3:4])
+print(sch[5:0:-1])
+
+print("Hello\nWorld...!")
+print("Hello\bWorld...!")
+print("Hello\tWorld...!")
+print("Hello\vWorld...!")
+'''
+
+'''
+function 내장함수
+method
+'''
+
+'''
+str_a = "하하하"
+print(type(str_a))
+str_a.replace("하", "호")
+print(str_a.replace("하", "호"))
+print(str_a.replace("하", "호", 1))
+
+str_c = "안녕하세요. 파이썬 수업입니다. 파이썬. 파이썬. 파이썬. 파이썬. 파이썬. 파이썬. 파이썬. 파이썬. 파이썬."
+print(str_c.replace("파이썬", "자바", 5))
+'''
+
+# 6자리 실수를 입력받는다(ex. 222.788)
+# 출력: 실수의 각 자리의 합을 출력한다. 2+2+2+7+8+8 => ??
+# input(), int(), str.replace()
+
+num = input("6자리 실수를 입력하시오")
+num = num.replace(".","")
+sum = int(num[0]) + int(num[1]) + int(num[2]) + int(num[3]) + int(num[4]) + int(num[5])
+print(sum)
